bd499d5cfaab186db1211c46adda9a226857ddcc
Samiha Tasnim <samiha@aparsclassroom.com>
2025-02-12 18:06:37 +0600

    minimum backbone create

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const app = express();
 app.use(bodyParser.json());
 
 app.use(cors({
-    origin: "http://localhost:3000",  // If using Vite
+    origin: "http://localhost:3000",
     credentials: true
   }));
 
@@ -19,20 +19,39 @@ mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopol
     .catch(err => console.error(err));
 
 // Define Schema
+
 const documentSchema = new mongoose.Schema({
+    year: {type: String, require: true},
     grade: { type: String, required: true },
     medium: {type: String, required: true},
-    subject: { type: String, required: true },
     version: { type: String, required: true },
-    drivePdfUrl: { type: String, required: true }
+    subject: { type: String, required: true },
+    drivePdfUrl: { type: String, required: false }
 });
 
 // Create Model
 const Document = mongoose.model('Document', documentSchema);
 
+const adminSchema = new mongoose.Schema({
+    email: { type: String, required: true, unique: true },
+    password: { type: String, required: true },
+})
+
+const Admin = mongoose.model('Admin', adminSchema)
+
+const userSchema = new mongoose.Schema({
+    email: { type: String, required: true, unique: true },
+    password: { type: String, required: true },
+}, {timestamps: true});
+
+const User = mongoose.model('User', userSchema)
+
 // Predefined login credentials
 const EMAIL = 'uploader@nctb.com';
 const PASSWORD = '123456';
+
+const ADMIN_EMAIL = 'admin@nctb.com'
+const ADMIN_PASSWORD = '123456'
 
 // Login Route
 app.post('/login', (req, res) => {
@@ -43,6 +62,56 @@ app.post('/login', (req, res) => {
         res.status(401).json({ success: false, message: 'Invalid credentials' });
     }
 });
+
+// admin login route
+app.post('/admin/login', async function (req, res) {
+    const { email, password, remember } = req.body;
+
+    if (email === ADMIN_EMAIL && password === ADMIN_PASSWORD) {
+        res.json({ success: true, message: 'Login successful', });
+    } else {
+        res.status(401).json({ success: false, message: 'Invalid credentials' });
+    }
+
+    // const admin = await Admin.findOne({ email });
+    // if (!admin) return res.status(401).json({ message: "User does not exist in records!" });
+    // const validPassword = await bcrypt.compare(password, admin.password);
+    // if (validPassword == false) return res.status(401).json({ message: "Invalid email or password" });
+    // if (remember == true) {
+    //   const token = jwt.sign({ admin }, process.env.JWT_SECRET);
+    //   return res.cookie('token', token, { maxAge: 86400000 }).status(200).json({ success: true, message: "Logged in successfully", admin });
+    // }
+  
+    // return res.json({ success: true, message: "Logged in successfully", admin });
+  })
+
+ app.get('/managers', async function (req, res) {
+  const managers = await user.find()
+  res.status(200).json([...managers])
+})
+
+
+app.post('/user/create', async (req, res) => {
+    const { email, pass, branch } = req.body;
+    const existingUser = await user.findOne({ email })
+    if (existingUser) {
+      return res.status(400).json({ success: false, message: "Manager already created using this email" });
+    }
+    const hashedPassword = await bcrypt.hash(pass, 10);
+    const manager = new user({ email, password: hashedPassword, branch });
+    await manager.save();
+    res.status(201).json({ success: true, message: "Manager created successfully" });
+  });
+  
+  
+  // delete branch manager
+  
+  app.post('/user/delete', async (req, res) => {
+    const { email } = req.body;
+    const manager = await user.findOneAndDelete({ email });
+    if (!manager) return res.status(404).json({ message: 'Manager not found', success: false });
+    return res.json({ success: true, message: 'Manager deleted successfully' });
+  })
 
 // POST route to add a new document
 app.post('/documents', async (req, res) => {
